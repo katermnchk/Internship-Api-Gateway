efd23b5ad25d26da8384a65b90a33f3a5642c018
Katya Remenchik <164913748+katermnchk@users.noreply.github.com>
2025-10-17 13:02:19 +0300

    init commit

---
+++ src/test/java/com/innowise/internship/InnowiseApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innowise.internship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnowiseApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
